9e1cd69deaed5408034f6900bdd5d23e3f1753b6
Gennady Oleshko <gennady@oleshko-g.ru>
2025-09-13 16:04:10 +0300

    implement POST /

--- main.go
+++ main.go
@@ -1,18 +1,69 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/base64"
+	"errors"
+	"io"
 	"log"
+	"mime"
 	"net/http"
+	"net/url"
 )
 
 const defaultTCPPort string = "8080"
 
-func minifyURL(res http.ResponseWriter, req *http.Request)   {}
-func unminifyURL(res http.ResponseWriter, req *http.Request) {}
-
 func init() {
-	http.HandleFunc("POST /", minifyURL)
+	http.HandleFunc("POST /", minifyURLHandler)
 	http.HandleFunc("GET /{id}", unminifyURL)
+}
+
+var minifiedURLs = make(map[string]string)
+
+func validateContenType(h http.Header, value string) error {
+	mediaType, _, errParseMediaType := mime.ParseMediaType(h.Get("Content-Type"))
+	if errParseMediaType != nil {
+		return errParseMediaType
+	}
+
+	if mediaType != value {
+		return errors.New("error: Content-Type isn't " + value)
+	}
+
+	return nil
+}
+
+func encode(data []byte) string {
+	checksum := md5.Sum(data)
+	return base64.RawURLEncoding.EncodeToString(checksum[:8])
+}
+
+func respondBadRequest(res http.ResponseWriter, err error) {
+	res.WriteHeader(http.StatusBadRequest)
+	log.Printf("error: %s", err)
+}
+
+func minifyURLHandler(res http.ResponseWriter, req *http.Request) {
+	err := validateContenType(req.Header, "text/plain")
+	if err != nil {
+		respondBadRequest(res, err)
+	}
+	log.Printf("%+v", req)
+
+	data, err := io.ReadAll(req.Body)
+	if err != nil {
+		respondBadRequest(res, err)
+	}
+
+	_, err = url.Parse(string(data))
+	if err != nil {
+		respondBadRequest(res, err)
+	}
+
+	minifiedURL := "http://" + req.Host + "/" + encode(data)
+	log.Printf("minifiedURL: %s", minifiedURL)
+	minifiedURLs[minifiedURL] = string(data)
+	res.WriteHeader(http.StatusCreated)
 }
 
 func main() {
