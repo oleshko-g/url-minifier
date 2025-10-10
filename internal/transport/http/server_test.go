cc4a89ffd4861dc9c8b7c8789fa4206e11e2646e
Gennady Oleshko <gennady@oleshko-g.ru>
2025-10-10 22:06:31 +0300

    add TestServer_minifyJSONURLHandler

--- internal/transport/http/server_test.go
+++ internal/transport/http/server_test.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"bytes"
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -72,7 +73,7 @@ func TestServer_minifyURLHandler(t *testing.T) {
 	}
 }
 
-func Test_unMinifyURLHandler(t *testing.T) {
+func TestServer_unMinifyURLHandler(t *testing.T) {
 	storage := memory.NewStrRecords()
 	service := minifier.New(storage)
 	service.Config.MaxLen = 8
@@ -118,6 +119,70 @@ func Test_unMinifyURLHandler(t *testing.T) {
 			require.NoError(t, err)
 
 			require.Equal(t, tt.want.headers["Location"], l.String())
+		})
+	}
+}
+
+func TestServer_minifyJSONURLHandler(t *testing.T) {
+	// setup server config to use it programmatically
+	storage := memory.NewStrRecords()
+	service := minifier.New(storage)
+	service.Config.MaxLen = 8
+	service.Config.BaseURL().Set("http://localhost:8080/")
+	server := NewServer(service)
+
+	tests := []struct {
+		name        string // description of this test case
+		originalURL string
+		minifiedID  string
+		want        testMinifierResponse
+	}{
+		{
+			name:        "Correct minify 'https://practicum.yandex.ru/'",
+			originalURL: "https://practicum.yandex.ru/",
+			minifiedID:  "DdGYF429IL4",
+			want: testMinifierResponse{
+				statusCode: 201,
+				headers: map[string]string{
+					"Content-Type":   "application/json",
+					"Content-Length": "46",
+				},
+				body: make([]byte, 0),
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// set wanted response body
+			resData, err := json.Marshal(minifyURLResponse{Result: service.BaseURL().String() + "/" + tt.minifiedID})
+			require.NoError(t, err)
+			tt.want.body = resData
+
+			// set up request
+			reqData, err := json.Marshal(minifyURLRequest{URL: tt.originalURL})
+			require.NoError(t, err)
+			b := bytes.NewBuffer(reqData)
+			req := httptest.NewRequest("POST", "/shorten/", b)
+			req.Header.Set("Content-Type", "application/json")
+			req.Header.Set("Content-Length", strconv.Itoa(b.Len()))
+
+			// make the request
+			w := httptest.NewRecorder()
+			server.minifyURLJSONHandler().ServeHTTP(w, req)
+			res := w.Result()
+			body, err := io.ReadAll(res.Body)
+			require.NoError(t, err)
+			defer res.Body.Close()
+
+			// tests
+			require.Equal(t, tt.want.statusCode, res.StatusCode)
+
+			for k, v := range tt.want.headers {
+				require.Equal(t, v, res.Header.Get(k), "Asserting %s: %s", k, v)
+			}
+
+			require.Equal(t, tt.want.body, body)
 		})
 	}
 }
