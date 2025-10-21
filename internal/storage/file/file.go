3e9faa0ce4675e510eff69ccc77ec3866f1c6ba6
Gennady Oleshko <gennady@oleshko-g.ru>
2025-10-21 21:01:28 +0300

    add file.New()

--- internal/storage/file/file.go
+++ internal/storage/file/file.go
@@ -1,9 +1,29 @@
 package file
 
-import "os"
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+)
+
+func New() *File {
+	zl := zerolog.New(os.Stderr).With().Timestamp().Logger()
+	file := File{
+		logger: zl,
+	}
+
+	fp, err := os.OpenFile(defaultPath+defaultFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, defaultPermissions)
+	if err != nil {
+		file.logger.Fatal().Msg(err.Error())
+	}
+	file.p = fp
+
+	return &file
+}
 
 type File struct {
-	*os.File
+	p      *os.File
+	logger zerolog.Logger
 }
 
 func (f *File) Save(key, value string) error {
@@ -13,3 +33,9 @@ func (f *File) Save(key, value string) error {
 func (f *File) Retrieve(key string) (value string, err error) {
 	return "", nil
 }
+
+const (
+	defaultPath                    = "./.files/"
+	defaultFileName                = "minifiedURLs.json"
+	defaultPermissions os.FileMode = 0o644 // UNIX persmiffions: WriteRread_, _Read_, _Read_
+)
