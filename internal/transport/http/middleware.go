ded2e9ed4c81bdd9b3b65ace0dc08a9305e0ca2b
Gennady Oleshko <gennady@oleshko-g.ru>
2025-11-01 19:48:31 +0300

    implement compressingResponseWriter.write()

--- internal/transport/http/middleware.go
+++ internal/transport/http/middleware.go
@@ -1,7 +1,6 @@
 package http
 
 import (
-	"bytes"
 	"compress/gzip"
 	"errors"
 	"fmt"
@@ -62,8 +61,15 @@ func (s *Server) withEncodingMiddleware(h http.HandlerFunc) http.HandlerFunc {
 		cw := compressingResponseWriter{
 			ResponseWriter: w,
 			coding:         compression,
-			buf:            &bytes.Buffer{},
+			compressor:     nil, // can't set compressor right away because it depends on the "Content-Type" of an HTTP response
 		}
+
+		// if the handler choose the compressor then compressor must be closed
+		defer func() {
+			if cw.compressor != nil {
+				cw.compressor.Close()
+			}
+		}()
 
 		h(&cw, req)
 	}
@@ -71,9 +77,13 @@ func (s *Server) withEncodingMiddleware(h http.HandlerFunc) http.HandlerFunc {
 
 type compressingResponseWriter struct {
 	http.ResponseWriter
-	buf        *bytes.Buffer
-	compressor io.WriteCloser
+	compressor writeFlushCloser
 	coding
+}
+
+type writeFlushCloser interface {
+	io.WriteCloser
+	Flush() error
 }
 
 // WriteHeader chooses a compressor based on the "Content-Type" of [http.ResponseWriter]
@@ -82,27 +92,34 @@ func (cw *compressingResponseWriter) WriteHeader(statusCode int) {
 	cw.ResponseWriter.WriteHeader(statusCode)
 }
 
+func (cw *compressingResponseWriter) Write(reponseBody []byte) (n int, err error) {
+	if cw.compressor != nil {
+		return cw.write(reponseBody)
+	}
+	return cw.ResponseWriter.Write(reponseBody)
+}
+
 // chooseCompressor might set the compressor and "Content-Encoding" of [http.ResponseWriter] based on its "Content-Type" [http.Header]
 func (cw *compressingResponseWriter) chooseCompressor() {
 	switch cw.ResponseWriter.Header().Get("Content-Type") {
 	case "application/json", "text/html":
 		switch cw.coding {
 		case codingGZIP:
-			cw.compressor = gzip.NewWriter(cw.ResponseWriter)
 			cw.ResponseWriter.Header().Set("Content-Encoding", string(cw.coding))
+			cw.compressor = gzip.NewWriter(cw.ResponseWriter)
 		case codingIdentity:
-			return
+			// no compression
 		}
-		return
 	}
 }
 
-func (cw *compressingResponseWriter) Write(b []byte) (n int, err error) {
+func (cw *compressingResponseWriter) write(dataToCompress []byte) (n int, err error) {
 	if cw.compressor != nil {
-		defer cw.compressor.Close()
-		return cw.compressor.Write(b)
+		n, err = cw.compressor.Write(dataToCompress)
+		cw.compressor.Flush() // need to close to Flush compressor's in memory buffer
+		return n, err
 	}
-	return cw.ResponseWriter.Write(b)
+	return 0, errors.New("compressor is nil")
 }
 
 func (s *Server) withLoggingMiddleware(h http.HandlerFunc) http.HandlerFunc {
@@ -136,14 +153,10 @@ type loggingResponseWriter struct {
 	contentLength int
 }
 
-func (lw *loggingResponseWriter) Write(data []byte) (n int, err error) {
-	n, err = lw.ResponseWriter.Write(data)
-	lw.contentLength = n
-	if err != nil {
-		return 0, err
-	}
-
-	return
+func (lw *loggingResponseWriter) Write(responseBody []byte) (n int, err error) {
+	n, err = lw.ResponseWriter.Write(responseBody)
+	lw.contentLength += n
+	return n, err
 }
 
 func (lw *loggingResponseWriter) WriteHeader(statusCode int) {
