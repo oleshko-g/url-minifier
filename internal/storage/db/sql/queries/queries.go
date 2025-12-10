e6f946005d5289fbf23b310ec785118c05e8793c
Gennady Oleshko <gennady@oleshko-g.ru>
2025-12-10 10:03:28 +0300

    add selectUserStrings
    Signed-off-by: Gennady Oleshko <gennady@oleshko-g.ru>

--- internal/storage/db/sql/queries/queries.go
+++ internal/storage/db/sql/queries/queries.go
@@ -12,3 +12,8 @@ var InsertString string
 //
 //go:embed selectString.sql
 var SelectString string
+
+// SelectString is the SQL statement to select all string by user_id from a db
+//
+//go:embed selectUserStrings.sql
+var SelectUserStrings string
---
+++ internal/storage/db/sql/queries/selectUserStrings.sql
@@ -0,0 +1,8 @@
+SELECT
+  id,
+  value
+FROM
+  strings
+WHERE
+  user_id = $1
+  AND deleted_at IS NOT NULL;
