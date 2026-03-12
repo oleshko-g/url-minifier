06e39a25956c9989042b4423fadf68a50a97a502
Gennady Oleshko <gennady@oleshko-g.ru>
2026-03-12 22:35:30 +0300

    Update linter.go
    Signed-off-by: Gennady Oleshko <gennady@oleshko-g.ru>

--- cmd/linter/linter.go
+++ cmd/linter/linter.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	_ "go/ast"
-	_ "go/token"
+	"go/ast"
 
 	"golang.org/x/tools/go/analysis"
 )
@@ -10,17 +9,38 @@ import (
 var PanicFatalExitAnalyzer *analysis.Analyzer = &analysis.Analyzer{
 	Name: "PanicFatalExitAnalyzer",
 	Doc:  "PanicFatalExit\n\nreports: [panic], [log.fatal] OR [os.exit]",
-	Run:  nil,
+	Run:  run,
 }
 
-func run(*analysis.Pass) (any, error) {
-	// TODO: feed packages
-	// TODO: parse files into AST
-	// TODO: traverse AST
+func run(pass *analysis.Pass) (any, error) {
+	inspect(pass.Files)
+	return nil, nil
+}
+
+func inspect(files []*ast.File) {
+	for _, file := range files {
+		ast.Inspect(file, func(node ast.Node) bool {
+			switch node := node.(type) {
+			case *ast.CallExpr:
+				check(node)
+			}
+			return true
+		})
+
+	}
+}
+
+func check(call *ast.CallExpr) *analysis.Diagnostic {
 	// TODO: write if CallExpr.Name == "panic" report the error
+
 	// TODO: if in FuncDecl.Name == "main" AND BlockStmt contains
 	// * TODO: write if log.Fatal.Name == "log.Fatal" report the error
 	// * TODO: write if log.Fatal.Name == "os.Exit" report the error
 	// # TODO: write if log.Fatal.Name == "os.Exit" report the error
-	return nil, nil
+
+	if ok := true; !ok {
+		return &analysis.Diagnostic{}
+	}
+
+	return nil
 }
