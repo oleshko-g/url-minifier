0769b71d1c34e683cd723845ab9a45119603e854
Gennady Oleshko <gennady@oleshko-g.ru>
2026-03-12 14:27:58 +0300

    write a skeleton for PanicFatalExit analyzer
    Signed-off-by: Gennady Oleshko <gennady@oleshko-g.ru>

--- cmd/linter/linter.go
+++ cmd/linter/linter.go
@@ -1,1 +1,26 @@
-package linter
+package main
+
+import (
+	_ "go/ast"
+	_ "go/token"
+
+	"golang.org/x/tools/go/analysis"
+)
+
+var PanicFatalExitAnalyzer *analysis.Analyzer = &analysis.Analyzer{
+	Name: "PanicFatalExitAnalyzer",
+	Doc:  "PanicFatalExit\n\nreports: [panic], [log.fatal] OR [os.exit]",
+	Run:  nil,
+}
+
+func run(*analysis.Pass) (any, error) {
+	// TODO: feed packages
+	// TODO: parse files into AST
+	// TODO: traverse AST
+	// TODO: write if CallExpr.Name == "panic" report the error
+	// TODO: if in FuncDecl.Name == "main" AND BlockStmt contains
+	// * TODO: write if log.Fatal.Name == "log.Fatal" report the error
+	// * TODO: write if log.Fatal.Name == "os.Exit" report the error
+	// # TODO: write if log.Fatal.Name == "os.Exit" report the error
+	return nil, nil
+}
