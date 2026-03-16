7dad96121110e0b7571e43925c21daa18d38d50d
Gennady Oleshko <gennady@oleshko-g.ru>
2026-03-16 23:51:37 +0300

    Update main.go
    Signed-off-by: Gennady Oleshko <gennady@oleshko-g.ru>

--- cmd/reset/main.go
+++ cmd/reset/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
 	"log"
+
 	"os"
 
 	"golang.org/x/tools/go/packages"
@@ -10,6 +12,7 @@ import (
 
 func main() {
 	dirPath := os.Args[1]
+	// dirPath := "../../"
 
 	pkgs, err := loadPackages(dirPath)
 	if err != nil {
@@ -18,20 +21,70 @@ func main() {
 
 	for _, pkg := range pkgs {
 		fmt.Println(pkg.Name)
-		for i, _ := range pkg.Syntax {
-			fmt.Println(pkg.CompiledGoFiles[i])
-		}
+		generateResetMethods(pkg)
 	}
 }
 
 func loadPackages(dirPath string) ([]*packages.Package, error) {
 	pkgs, err := packages.Load(&packages.Config{
 		Dir:  dirPath,
-		Mode: packages.NeedName | packages.LoadSyntax | packages.LoadFiles,
+		Mode: packages.NeedName | packages.LoadSyntax | packages.NeedModule,
 	}, "./...")
 	if err != nil {
 		return nil, err
 	}
 
 	return pkgs, nil
+}
+
+func generateResetMethods(pkg *packages.Package) error {
+	for _, file := range pkg.Syntax {
+		structs := structsToReset(file)
+		if structs == nil {
+			return nil
+		}
+	}
+
+	return nil
+}
+
+func structsToReset(file *ast.File) map[string]*ast.StructType {
+	var structsToReset = make(map[string]*ast.StructType)
+
+	for _, decl := range file.Decls {
+		genDecl, ok := decl.(*ast.GenDecl)
+		if ok {
+			if hasComment("// generate:reset", genDecl) {
+				if name, structType := isStructDecl(genDecl); structType != nil {
+					structsToReset[name] = structType
+				}
+			}
+		}
+	}
+
+	return structsToReset
+}
+
+func isStructDecl(decl *ast.GenDecl) (name string, typ *ast.StructType) {
+	if len(decl.Specs) == 1 {
+		if typeSpec, ok := decl.Specs[0].(*ast.TypeSpec); ok {
+			if structType, ok := typeSpec.Type.(*ast.StructType); ok {
+				return typeSpec.Name.Name, structType
+			}
+		}
+	}
+
+	return "", nil
+}
+
+func hasComment(comment string, decl *ast.GenDecl) bool {
+	if decl.Doc != nil {
+		for _, doc := range decl.Doc.List {
+			if doc.Text == comment {
+				return true
+			}
+		}
+	}
+
+	return false
 }
