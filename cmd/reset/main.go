0547262e7c0b82e49391da647ac42255ef1e8931
Gennady Oleshko <gennady@oleshko-g.ru>
2026-03-16 22:10:52 +0300

    add loadPackages
    Signed-off-by: Gennady Oleshko <gennady@oleshko-g.ru>

--- cmd/reset/main.go
+++ cmd/reset/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"golang.org/x/tools/go/packages"
+)
+
+func main() {
+	dirPath := os.Args[1]
+
+	pkgs, err := loadPackages(dirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, pkg := range pkgs {
+		fmt.Println(pkg.Name)
+		for i, _ := range pkg.Syntax {
+			fmt.Println(pkg.CompiledGoFiles[i])
+		}
+	}
+}
+
+func loadPackages(dirPath string) ([]*packages.Package, error) {
+	pkgs, err := packages.Load(&packages.Config{
+		Dir:  dirPath,
+		Mode: packages.NeedName | packages.LoadSyntax | packages.LoadFiles,
+	}, "./...")
+	if err != nil {
+		return nil, err
+	}
+
+	return pkgs, nil
 }
