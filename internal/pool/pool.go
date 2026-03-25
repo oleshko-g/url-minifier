1d7407f2df4b9ba4da95140fdfe42d56b94df400
Gennady Oleshko <gennady@oleshko-g.ru>
2026-03-26 00:31:17 +0300

    Create pool.go
    Signed-off-by: Gennady Oleshko <gennady@oleshko-g.ru>

---
+++ internal/pool/pool.go
@@ -0,0 +1,43 @@
+// Package pool provides generic [Pool] of structs that can Reset() themselves before returning into the pool
+package main
+
+import (
+	"sync"
+
+	"github.com/oleshko-g/url-minifier/cmd/reset/example"
+)
+
+func New[R resetter]() *Pool[R] {
+	return &Pool[R]{
+		internal: sync.Pool{},
+	}
+}
+
+// Pool is a generic struct that can reset itself
+type Pool[T resetter] struct {
+	internal sync.Pool
+}
+
+func (p *Pool[T]) Get() T {
+	return p.internal.Get().(T)
+}
+
+func (p *Pool[T]) Put(t T) {
+	p.internal.Put(t)
+}
+
+type resetter interface {
+	Reset()
+}
+
+func f() {
+	pool := New[*example.GenStruct]()
+	nill := pool.Get()
+	pool.Put(&example.GenStruct{})
+
+	_, _ = pool, nill
+}
+
+func main() {
+	f()
+}
