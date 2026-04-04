cc012da0736fe93ab25c69e9082dbfa1c89eb2c6
Gennady Oleshko <gennady@oleshko-g.ru>
2026-04-04 11:54:06 +0300

    add X-Real-IP check

--- internal/transport/http/stats.go
+++ internal/transport/http/stats.go
@@ -1,13 +1,36 @@
 package http
 
-import "net/http"
+import (
+	"net"
+	"net/http"
+)
 
 func (s *Server) statsHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// * TODO: check request header X-Trusted-Subnet
-		// * TODO: go getNumberOfMinifiedURLs
-		// * TODO: go getNumberOfUsers
+		// * TODO: check trusted subnet
 		defer r.Body.Close()
+
+		realIPString := r.Header.Get("X-Real-IP")
+		if realIPString == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		IPAdress, IPsubnet, err := net.ParseCIDR(realIPString)
+		if err != nil {
+			s.logger.Error(err.Error())
+			responseWithError(w, err, http.StatusInternalServerError)
+			return
+		}
+
+		_, _ = IPAdress, IPsubnet
+
+		// * TODO: check request header X-Trusted-Subnet
+
+		// * TODO: go getNumberOfMinifiedURLs
+
+		// * TODO: go getNumberOfUsers
+
 		w.WriteHeader(http.StatusNotImplemented)
 	}
 }
