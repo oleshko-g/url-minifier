25c51556906eedab416b1919fcd4fe8ab923ca07
Gennady Oleshko <gennady@oleshko-g.ru>
2026-04-08 13:59:21 +0700

    refactor grpc.New

--- internal/transport/grpc/server.go
+++ internal/transport/grpc/server.go
@@ -5,25 +5,32 @@ import (
 
 	"github.com/oleshko-g/url-minifier/internal/transport/config"
 	minifier_v1 "github.com/oleshko-g/url-minifier/proto/api/v1"
+	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type Server struct {
+	*grpc.Server
+}
+
+type server struct {
 	minifier_v1.UnimplementedMinifierServiceServer
 }
 
-func New(cfg *config.Config) *Server {
+func NewServer(cfg *config.Config) *Server {
+	srv := grpc.NewServer()
+	minifier_v1.RegisterMinifierServiceServer(srv, &server{})
 	return &Server{}
 }
 
-func (s *Server) MinifyURL(ctx context.Context, req *minifier_v1.MinifyURLRequest) (*minifier_v1.MinifyURLResponse, error) {
+func (s *server) MinifyURL(ctx context.Context, req *minifier_v1.MinifyURLRequest) (*minifier_v1.MinifyURLResponse, error) {
 	return s.UnimplementedMinifierServiceServer.MinifyURL(ctx, req)
 }
 
-func (s *Server) UnminifyURL(ctx context.Context, req *minifier_v1.UnminifyURLRequest) (*minifier_v1.UnminifyURLResponse, error) {
+func (s *server) UnminifyURL(ctx context.Context, req *minifier_v1.UnminifyURLRequest) (*minifier_v1.UnminifyURLResponse, error) {
 	return s.UnimplementedMinifierServiceServer.UnminifyURL(ctx, req)
 }
 
-func (s *Server) ListUserURLs(ctx context.Context, req *emptypb.Empty) (*minifier_v1.UserURLsResponse, error) {
+func (s *server) ListUserURLs(ctx context.Context, req *emptypb.Empty) (*minifier_v1.UserURLsResponse, error) {
 	return s.UnimplementedMinifierServiceServer.ListUserURLs(ctx, req)
 }
