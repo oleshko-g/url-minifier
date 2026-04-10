4f10e92e7d1f602d214c396209544619bc81fbfc
Gennady Oleshko <gennady@oleshko-g.ru>
2026-04-10 23:17:26 +0700

    regenerate mock/service/minifier.go

--- internal/mock/service/minifier.go
+++ internal/mock/service/minifier.go
@@ -5,10 +5,9 @@ package minifier
 
 import (
 	"context"
-	"sync"
-
 	"github.com/oleshko-g/url-minifier/internal/service"
 	"github.com/oleshko-g/url-minifier/internal/service/minifier"
+	"sync"
 )
 
 // Ensure, that MinifierMock does implement service.Minifier.
